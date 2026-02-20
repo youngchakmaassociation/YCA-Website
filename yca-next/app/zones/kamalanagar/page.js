fb66b8e13e4ef1ff6c33a96d611df2ad623f05ee
YCA Admin <youngchakma1978@gmail.com>
2026-02-20 12:42:25 +0530

    Fix: Remove server-side config from client components causing runtime error

--- yca-next/app/zones/kamalanagar/page.js
+++ yca-next/app/zones/kamalanagar/page.js
@@ -1,8 +1,5 @@
 'use client';
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
